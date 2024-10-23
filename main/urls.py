864964aa509031ec820163fe031b47db79879ce2
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-10-23 21:50:22 +0100

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -14,10 +14,13 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('guest/', views.guest, name='guest'),
     path('home/', views.home, name='home'),
+    path('info/', views.info, name='info'),
     path('chart/<str:ticker>/', views.chart, name='chart'),
     path('backtest_chart/<str:ticker>/<str:type>/<str:interval>/<str:start>/<str:end>/', views.backtest_chart, name='backtest_chart'),
     path('', include(router.urls)),
     path('get_data/<str:ticker>/', get_data, name='get_data'),
     path('get_backtest_data/<str:ticker>/<str:type>/<str:interval>/<str:start>/<str:end>/', get_backtest_data, name='get_backtest_data'),
+    path('add-to-favorites/', views.add_to_favorites, name='add_to_favorites'),
+    path('get-favorites/', views.get_favorites, name='get_favorites'),
 ]
 
